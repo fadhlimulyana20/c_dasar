9742a00645555563265d97fd85f314411f8311e9
fadhlimulyana20 <baba.fadhli@gmail.com>
2019-09-21 13:37:31 +0700

    Update luas.c
    Memisah input dan output luas serta volum

--- luas.c
+++ luas.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
 
 int main(){
-	int alas, tinggi, panjang1, panjang2, lebar, radius;
-	float phi = 3.14;
-	//input data
+        //input buat cari luas segi3, pesegi, persegi panjang
+
+	int alas, tinggi, panjang1, panjang2, lebar;
+
 	printf("Masukkan nilai alas\t:");
 	scanf("%d", &alas);
 	printf("Masukkan nilai tinggi\t:");
@@ -14,20 +15,29 @@ int main(){
 	scanf("%d", &panjang2);
 	printf("Masukkan nilai lebar persegi panjang\t:");
 	scanf("%d", &lebar);
+
+        //Output buat luas
+        int luas_segi3 = alas*tinggi/2;
+	int luas_persegi = panjang1*panjang1;
+	int luas_persegi_panjang = lebar*panjang2;
+
+        //Print data luas
+        printf("Luas segitiga\t:%d\n", luas_segi3);
+	printf("Luas persegi\t:%d\n", luas_persegi);
+	printf("Luas persegi panjang\t:%d\n", luas_persegi_panjang);
+
+        //Input data volum bola
+        float phi = 3.14;
+        int radius;
+
 	printf("Masukkan nilai jari-jari\t:");
 	scanf("%d", &radius);
 
-	//output data
-	int luas_segi3 = alas*tinggi/2;
-	int luas_persegi = panjang1*panjang1;
-	int luas_persegi_panjang = lebar*panjang2;
+	//output data volum bola
 	float volume_lingkaran = (4/3)*phi*(radius*radius*radius);
 
 
 	//print data
-	printf("Luas segitiga\t:%d\n", luas_segi3);
-	printf("Luas persegi\t:%d\n", luas_persegi);
-	printf("Luas persegi panjang\t:%d\n", luas_persegi_panjang);
-	printf("Volum Balok\t:%.2f", volume_lingkaran);
+        printf("Volum bola\t:%.2f", volume_lingkaran);
 
 }
