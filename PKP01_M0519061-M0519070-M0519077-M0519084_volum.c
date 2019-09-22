275f83ffb488c08ff3b671a584ddfa7dca0a1fda
fadhlimulyana20 <baba.fadhli@gmail.com>
2019-09-22 12:32:57 +0700

    Update PKP01_M0519061-M0519070-M0519077-M0519084_volum.c
    editing pow function to calculate sphere volume

--- PKP01_M0519061-M0519070-M0519077-M0519084_volum.c
+++ PKP01_M0519061-M0519070-M0519077-M0519084_volum.c
@@ -11,7 +11,7 @@ int main(){
 	scanf("%d", &radius);
 	
 	//menghitung nilai volum bola
-	float volum_bola = (4/3)*phi*(radius^3);
+	float volum_bola = (4/3)*phi*(pow(radius, 3);
 	
 	//output data
 	printf("Nilai Volum Vola\t: %.2f", volum_bola);
