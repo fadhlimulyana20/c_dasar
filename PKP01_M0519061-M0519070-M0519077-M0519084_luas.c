2a049f83b6b241595ad658c2fd4d87d11d4562ee
Shira26 <55570540+Shira26@users.noreply.github.com>
2019-09-22 15:15:30 +0700

    Update PKP01_M0519061-M0519070-M0519077-M0519084_luas.c

--- PKP01_M0519061-M0519070-M0519077-M0519084_luas.c
+++ PKP01_M0519061-M0519070-M0519077-M0519084_luas.c
@@ -1,15 +1,16 @@
 #include <stdio.h>
+#include <math.h>
 
 int main(){
 	//deklarasi variabel
-	int alas, tinggi, panjang1, panjang2, lebar;
+	int jari2, panjang1, panjang2, lebar;
+	float phi;
+	
+	phi = 3.14;
 	
 	//input data
-	printf("Masukkan nilai alas\t:");
-	scanf("%d", &alas);
-	
-	printf("Masukkan nilai tinggi\t:");
-	scanf("%d", &tinggi);
+	printf("Masukkan nilai jari-jari lingkaran\t:");
+	scanf("%d", &jari2);
 	
 	printf("Masukkan nilai panjang persegi\t:");
 	scanf("%d", &panjang1);
@@ -21,12 +22,12 @@ int main(){
 	scanf("%d", &lebar);
 	
 	//menghitung luas
-	int luas_segi3 = alas*tinggi/2;
+	float luas_lingkaran = phi*(pow(jari2,2));
 	int luas_persegi = panjang1*panjang1;
 	int luas_persegi_panjang = lebar*panjang2;
 	
 	//output data
-	printf("Luas segitiga\t:%d\n", luas_segi3);
+	printf("Luas lingkaran\t:%.2f\n", luas_lingkaran);
 	printf("Luas persegi\t:%d\n", luas_persegi);
 	printf("Luas persegi panjang\t:%d\n", luas_persegi_panjang);
 }
