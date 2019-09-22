e5bd328f2d8a6fb6562fc56af2d7bd6ca3394c88
sindynur67 <55570387+sindynur67@users.noreply.github.com>
2019-09-22 14:51:35 +0700

    Update PKP01_M0519061-M0519070-M0519077-M0519084_volum.c

--- PKP01_M0519061-M0519070-M0519077-M0519084_volum.c
+++ PKP01_M0519061-M0519070-M0519077-M0519084_volum.c
@@ -3,6 +3,7 @@
 
 int main(){
 	float phi, radius;
+	int panjang1, lebar1, tinggi1, panjang2, lebar2, tinggi2;
 	
 	phi = 3.14;
 	
@@ -10,10 +11,31 @@ int main(){
 	printf("Masukkan nilai radius\t:");
 	scanf("%f", &radius);
 	
+	printf("Masukkan nilai panjang1\t:");
+	scanf("%d", &panjang1);
+	
+	printf("Masukkan nilai lebar1\t:");
+	scanf("%d", &lebar1);
+	
+	printf("Masukkan nilai tinggi1\t:");
+	scanf("%d", &tinggi1);
+	
+	printf("Masukkan nilai panjang2\t:");
+	scanf("%d", &panjang2);
+	
+	printf("Masukkan nilai lebar2\t:");
+	scanf("%d", &lebar2);
+	
+	printf("Masukkan nilai tinggi2\t:");
+	scanf("%d", &tinggi2);
+		
 	//menghitung nilai volum bola
-	float volum_bola = (4*phi*(pow(radius, 3)))/3;
+	float volume_bola = (4*phi*(pow(radius, 3)))/3;
+	int volume_balok = panjang1*lebar1*tinggi1;
+	int volume_limas = panjang2*lebar2*tinggi2/2;
 	
 	//output data
-	printf("Nilai Volum Vola\t: %.2f", volum_bola);
-	
+	printf("Volume Vola\t: %.2f\n", volume_bola);
+	printf("Volume balok\t:%d\n", volume_balok);
+	printf("Volume limas\t:%d\n", volume_limas);
 }
