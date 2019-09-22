70fb2aae0ec1a252da6ad2d71ce93922af5c6d86
fadhlimulyana20 <baba.fadhli@gmail.com>
2019-09-22 07:29:54 +0700

    Add files via upload
    Source code volume bola

---
+++ PKP01_M0519061_volum_bola.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main(){
+	float phi;
+	int radius;
+	
+	phi = 3.14;
+	
+	//input data
+	printf("Masukkan nilai radius\t:");
+	scanf("%d", &radius);
+	
+	//menghitung nilai volum bola
+	float volum_bola = (4/3)*phi*(radius^3);
+	
+	//output data
+	printf("Nilai Volum Vola\t: %.2f", volum_bola);
+}
