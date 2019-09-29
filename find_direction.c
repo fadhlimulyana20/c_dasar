49f763074b92a724a4b4e95f07cfb18706beb4ab
fadhlimulyana20 <baba.fadhli@gmail.com>
2019-09-29 13:49:32 +0700

    Updating find direction program

--- find_direction.c
+++ find_direction.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <math.h>
+#include <string.h>
 #define pi 3.14
 
 int main(){
@@ -18,4 +19,26 @@ int main(){
     result = asin(sin)*val;
     
     printf("Inverse of sin(%.2f) = %.2lf in degrees\n", sin, result);
+
+    char arah[12];
+
+    if (x>0 && y==0){
+        strcpy(arah, "Timur");
+    }else if (x>0 && y>0){
+        strcpy(arah, "Timur_Laut");
+    }else if (x == 0 && y>0){
+        strcpy(arah, "Utara");
+    }else if (x<0 && y>0){
+        strcpy(arah, "Barat_Laut");
+    }else if (x<0 && y==0){
+        strcpy(arah, "Barat");
+    }else if (x<0 && y<0){
+        strcpy(arah, "Barat_daya");
+    }else if (x==0 && y<0){
+        strcpy(arah, "Selatan");
+    }else if (x>0 && y<0){
+        strcpy(arah, "Tenggara");
+    }
+
+    printf("Arah gerak vektor %s\n", arah);
 }
