52f3a2b7c426a6accd02be59be6769c4e649c5d7
fadhlimulyana20 <baba.fadhli@gmail.com>
2019-10-01 09:15:33 +0700

    Update nomer_2.c

--- nomer_2.c
+++ nomer_2.c
@@ -1,38 +1,36 @@
 #include<stdio.h>
 
 int x, y;
-
 int main (){
+    printf("Nilai x:");
+    scanf("%d", &x);
 
-printf("Nilai x:");
-scanf("%d", &x);
+    printf("Nilai y:");
+    scanf("%d", &y);
 
-printf("Nilai y:");
-scanf("%d", &y);
-
-if (x==0 && y>0){
-    printf("Arah\t: Utara");
-}
-else if (x>0 && y==0){
-    printf("Arah\t: Timur");
-}
-else if (x==0 && y<0){
-    printf("Arah\t: Selatan");
-}
-else if (x<0 && y==0){
-    printf("Arah\t: Barat");
-}
-else if (x>0 && y>0){
-    printf("Arah\t: Timur Laut");
-}
-else if (x>0 && y<0){
-    printf("Arah\t: Tenggara");
-}
-else if (x<0 && y<0){
-    printf("Arah\t: Barat Daya");
-}
-else if (x<0 && y>0){
-    printf("Arah\t: Barat Laut");
-}
+    if (x==0 && y>0){
+        printf("Arah\t: Utara");
+    }
+    else if (x>0 && y==0){
+        printf("Arah\t: Timur");
+    }
+    else if (x==0 && y<0){
+        printf("Arah\t: Selatan");
+    }
+    else if (x<0 && y==0){
+        printf("Arah\t: Barat");
+    }
+    else if (x>0 && y>0){
+        printf("Arah\t: Timur Laut");
+    }
+    else if (x>0 && y<0){
+        printf("Arah\t: Tenggara");
+    }
+    else if (x<0 && y<0){
+        printf("Arah\t: Barat Daya");
+    }
+    else if (x<0 && y>0){
+        printf("Arah\t: Barat Laut");
+    }
 }
 
